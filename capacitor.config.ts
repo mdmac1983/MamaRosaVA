0c73cf1c7254793075a686c25f9b5ec213d7c24f
mdmac2683 <mdmac1983@proton.me>
2026-08-29 06:02:31 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mamarosas.app',
-  appName: "Mama Rosa's",
-  webDir: 'www'
-};
-
-export default config;
